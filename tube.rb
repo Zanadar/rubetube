a4dcb06d9c67ef71e8a3c5c1a8de353d16a8f6de
Zander Mackie <zmackie@gmail.com>
2015-03-20 16:52:18 -0400

    Basic refactor to add Connection class

--- tube.rb
+++ tube.rb
@@ -9,18 +9,35 @@ class Tube
     loop do
       socket = @server.accept
 
-      data = socket.readpartial(1024)
-      puts data
-      
-      socket.write "HTTP/1.1 200 OK\r\n"
-      socket.write "\r\n"
-      socket.write "Hello\n"
+      connection = Connection.new(socket)
+      connection.process
+    end
+  end
 
-      socket.close
+  class Connection
+    def initialize(socket)
+      @socket = socket
+    end
+
+    def process
+      data = @socket.readpartial(1024)
+      puts data
+      send_response
+    end
+    
+    def send_response    
+      @socket.write "HTTP/1.1 200 OK\r\n"
+      @socket.write "\r\n"
+      @socket.write "Hello\n"
+      
+      socket_close
+    end
+
+    def socket_close
+      @socket.close
     end
   end
 end
-
 
 server = Tube.new(3000)
 puts  "Plugging into the <Tube>"
